dc8b2c92e86ce9dc754c7b70b3e656044b8f6ce5
NVV Ramana Reddy <01ramanareddy@gmail.com>
2025-03-09 17:36:00 +0530

    added dotenv

--- UserSide/config.jsx
+++ UserSide/config.jsx
@@ -1,2 +1,7 @@
-const BASE_URL = "http://prasthan-git-main-ramana-reddys-projects-a8b535ac.vercel.app"; // Replace with your IP
+
+
+const BASE_URL = __DEV__
+  ? "http://localhost:4000" // For Android Emulator
+  : "https://prasthan-git-main-ramana-reddys-projects-a8b535ac.vercel.app"; // Production API
+
 export default BASE_URL;
